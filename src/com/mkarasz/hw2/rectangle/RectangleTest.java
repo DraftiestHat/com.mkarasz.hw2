0658ae974535dbcee038ef1541e692213c1a6a07
DraftiestHat <DraftiestHat@users.noreply.github.com>
2015-02-11 14:29:00 -0700

    FInal update to RectangleTest.java
    Hopefuly this works. Did last night, but for some reason didn't apply.

--- src/com/mkarasz/hw2/rectangle/RectangleTest.java
+++ src/com/mkarasz/hw2/rectangle/RectangleTest.java
@@ -1,53 +1,14 @@
-<<<<<<< HEAD
+/**
+ * Tests the Rectangle constructor. All methods will be called from in here.
+ */
+
 package com.mkarasz.hw2.rectangle;
 
 
 
 public class RectangleTest {
 
-	public static void main(String[] args) throws InterruptedException {
-		Rectangle r = new Rectangle();
-		
-		
-		System.out.println("Non-initialized-Rectangle stats:");
-		printStats(r);
-		System.out.println();
-		r.draw();
-		
-		r = new Rectangle(70,45.2);
-		System.out.println("Width and Height-initialized-Rectangle stats:");
-		printStats(r);
-		System.out.println();
-		r.draw();
-		
-		r = new Rectangle(8.1, 9.2, 2.6, 3.0);
-		System.out.println("Everything-initialized-Rectangle stats:");
-		printStats(r);
-		r.draw();
-		
-		Thread.sleep(20000);
-		
-		System.exit(0);
-		
-	}
-	
-	public static void printStats(Rectangle r){
-		System.out.printf("The upper left corner of the rectangle is at (%f,%f).", r.getX(), r.getY());
-		System.out.println("The rectangle is " + r.getWidth() + " by " + r.getHeight());
-		System.out.println("It has area " + r.area() + " and perimeter " + r.perimeter());
-		System.out.println("The rectangles diagonal is of length " + r.diagonalLength());
-		System.out.println("The rectangles upper left corner is " + r.distanceFromOrigin() + " from the origin.");
-	}
-
-}
-=======
-package com.mkarasz.hw2.rectangle;
-
-
-
-public class RectangleTest {
-
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		Rectangle r = new Rectangle();
 		
 		
@@ -68,10 +29,13 @@ public class RectangleTest {
 		r.draw();
 		
 		
-		System.exit(0);
 		
 	}
 	
+	/**
+	 * Prints the stats of the given Rectangle object.
+	 * @param r
+	 */
 	public static void printStats(Rectangle r){
 		System.out.printf("The upper left corner of the rectangle is at (%f,%f).", r.getX(), r.getY());
 		System.out.println("The rectangle is " + r.getWidth() + " by " + r.getHeight());
@@ -81,4 +45,3 @@ public class RectangleTest {
 	}
 
 }
->>>>>>> origin/master
